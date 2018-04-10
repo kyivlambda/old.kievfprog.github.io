8476dc7d2a15aad87f091bd527b4b61b88a384c5
Vlad Ki <vlad@kirillov.im>
2018-04-10 12:34:43 +0300

    going markdown!

--- render.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env PYTHONIOENCODING=utf-8 python2.7
-
-import sys
-import pystache
-import yaml
-
-uread = lambda x: unicode(x.read().decode('utf-8'))
-print pystache.render(uread(sys.stdin), yaml.load(uread(open(sys.argv[1]))))
